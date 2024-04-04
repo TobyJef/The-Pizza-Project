eec8e7b9496264aaa84f983712d626805fa0f8d0
Toby Jefferies <toby_jefferies@hotmail.com>
2024-04-04 22:38:49 +0000

    add and migrate summernote model, class and decorator to admin.py

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
+
+#summernote decorator for customer reviews
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+#summernote decorator for customer reviews
 
 
-# Register your models here.
+

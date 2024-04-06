1df5bd3e58afb682897346eabb15f15082cc17d9
Toby Jefferies <toby_jefferies@hotmail.com>
2024-04-06 15:07:43 +0000

    added new folders for future image documentation and site images

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
-
+from django.views import generic
+from .models import Post
 
 
 # Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('created_on')
+    template_name = 'index.html'
+    paginate_by = 5

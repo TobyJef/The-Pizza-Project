071afa29d308dc4c418b3ba4a6de9fb887afe556
Toby Jefferies <toby_jefferies@hotmail.com>
2024-04-06 21:58:46 +0000

    add template inheritance to index, menu and review pages

--- run.py
+++ run.py
@@ -1,11 +1,27 @@
 import os
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
+
+# route decorator for home page
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# route decorator for menu page
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+
+# route decorator for review page
+@app.route("/reviews")
+def reviews():
+    return render_template("post_review.html")
+
 
 if __name__ == "__main__":
     app.run(

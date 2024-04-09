d82bcc5fcabccce93ee9d7a55a75c22d0300c617
Toby Jefferies <toby_jefferies@hotmail.com>
2024-04-09 14:00:30 +0000

    get project working following move to CI gitpod user

--- pizzaproject/settings.py
+++ pizzaproject/settings.py
@@ -24,12 +24,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qdweh2on4=t)jb5og0@6tagyj)yiz7*gt7k55((@*&+1=wfl7z'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-tobyjef-thepizzaproject-6440appxd8y.ws-eu110.gitpod.io', 'the-pizza-project-706f358fb150.herokuapp.com', 'the-pizza-project.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-tobyjef-thepizzaproject-6440appxd8y.ws-eu110.gitpod.io', '8000-tobyjef-thepizzaproject-2jwl132qdn1.ws-eu110.gitpod.io', 'the-pizza-project-706f358fb150.herokuapp.com', 'the-pizza-project.herokuapp.com', 'localhost']
 
 
 # Application definition

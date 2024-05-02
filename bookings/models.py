a741e4e0222a5067149f492c7784355a3a4c4070
Toby Jefferies <toby_jefferies@hotmail.com>
2024-05-02 22:56:18 +0000

    alter booking model for titles and user

--- bookings/models.py
+++ bookings/models.py
@@ -1,12 +1,27 @@
 from django.db import models
+from datetime import datetime
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+
+Title = (
+    ("Mr", "Mr"),
+    ("Mrs", "Mrs"),
+    ("Miss", "Miss"),
+    ("Ms", "Ms"),
+    ("Dr", "Dr"),
+    ("Prof", "Prof"),
+)
+
+
+
 
 # Customer Booking Model
 
 class Booking(models.Model):
 
-    title = models.CharField(max_length=10)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=10, choices=Title, blank=True)
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     email = models.EmailField(max_length=100)
@@ -17,3 +32,5 @@ class Booking(models.Model):
     allergies = models.CharField(max_length=150, default="No Allergies Stated")
     booking_size = models.IntegerField(null=True)
     additional_requests = models.CharField(max_length=600, default="No Additional Request")
+    def __str__(self):
+        return f"{self.user.username} | booking_date: {self.booking_date} | time: {self.booking_time}"
--- bookings/views.py
+++ bookings/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from datetime import datetime, timedelta
 from django.http import HttpResponse
 from django.views import generic, View
 from .models import Booking
-
+from django.contrib import messages
 
 
 # Home Page View
@@ -26,4 +27,27 @@ def enquiries(request):
         )
 # /enquiries page view
 
+def booking(request):
+    # 
+    day = validWeekday(94)
 
+
+    # 
+    validateWeekdays = isWeekdayValid(weekdays)
+
+
+    if request.method == 'POST':
+        booking_date = request.POST.get('booking_date')
+        booking_time = request.POST.get('booking_time')
+        if booking_date or booking_time == None:
+            messages.success(request, "Please select a Date")
+            return redirect('bookings')
+
+        
+        request.session['booking_date'] = booking_date
+        request.session['booking_time'] = booking_time
+
+        return redirect('bookingsSubmit')
+
+
+    return render(request, 'bo')

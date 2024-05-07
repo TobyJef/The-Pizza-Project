3739b820f49e2785910d5f8d312cc847dfa426ed
Toby Jefferies <toby_jefferies@hotmail.com>
2024-05-07 14:45:57 +0000

    begin securing unathaurised access to user views

--- bookings/views.py
+++ bookings/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template import loader
 from datetime import datetime, timedelta
 from django.http import HttpResponse
 from django.views import generic, View
 from .models import Booking
 from django.contrib import messages
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views.generic.edit import FormView, UpdateView, DeleteView
 
 # Home Page View
@@ -29,12 +31,27 @@ def enquiries(request):
 
 # User Booking View
 def user_bookings(request):
-    mybookings = Booking.objects.all().values()
-    template = loader.get_template('user_bookings.html')
-    context = { 'mybookings' : mybookings}
+    return render (
+        request, 'user_bookings.html'
+    )
 
-    return HttpResponse (template.render(context, request))
-        
 # /user booking view
 
 
+class BookingAmend(UserPassesTestMixin, UpdateView):
+    model = Booking
+    template_name = 'amend_booking.html'
+    fields = ['booking_date', 'start_time', 'end_time', 'address', 'dietary_requirements', 'booking_size']
+    success_url ='/', 'user_booking.html'
+
+    def get_queryset(self):
+        return self.request.user.booking_set.all()
+
+
+class BookingDelete(UserPassesTestMixin, DeleteView):
+    model = Booking
+    template_name = 'delete_booking.html'
+    success_url ='/', 'user_booking.html'
+
+    def get_queryset(self):
+        return self.request.user.booking_set.all()
